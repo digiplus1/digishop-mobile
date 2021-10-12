d09bbef810be4ee8ace4444cafdbfd127889e3f6
fokou87 <ghislain.fokou@digiplusconsulting.com>
2021-10-12 11:22:38 +0100

    first commit

--- src/app/Service/AdresseIP.ts
+++ src/app/Service/AdresseIP.ts
@@ -1,4 +1,5 @@
 export class AdresseIP {
-    static host="http://localhost:9009/";
+  //  static host="http://localhost:9009/";
+  static host="https://digishopbackend-staging.herokuapp.com/"
   //static host_cotation="https://digixpressonline.herokuapp.com/api/cotation_partner/";
 }
